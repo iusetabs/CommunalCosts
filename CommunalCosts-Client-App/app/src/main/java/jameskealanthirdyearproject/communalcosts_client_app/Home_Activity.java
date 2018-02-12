190991804aafcf806f5524978de52587d4ba36a3
Nolan <j.nolan@sap.com>
2018-02-12 14:30:12 +0000

    can you fix the dialog in home_actv?

--- CommunalCosts-Client-App/app/src/main/java/jameskealanthirdyearproject/communalcosts_client_app/Home_Activity.java
+++ CommunalCosts-Client-App/app/src/main/java/jameskealanthirdyearproject/communalcosts_client_app/Home_Activity.java
@@ -1,10 +1,12 @@
 package jameskealanthirdyearproject.communalcosts_client_app;
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -13,9 +15,12 @@ import com.google.firebase.auth.FirebaseUser;
 public class Home_Activity extends AppCompatActivity implements View.OnClickListener{
 
     private Button logOutBtn;
+    private Button joinCollBtn;
     private FirebaseAuth firAuth;
     private Intent logInActivity;
     private TextView welcomeText;
+    private AlertDialog.Builder addCollectiveDia;
+    private View mView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +29,7 @@ public class Home_Activity extends AppCompatActivity implements View.OnClickList
 
         logInActivity = new Intent(Home_Activity.this, LogIn_Activity.class);
         logOutBtn = (Button) findViewById(R.id.Home_LogOutBtn);
+        joinCollBtn = (Button) findViewById(R.id.home_JoinCollectiveBtn);
 
         welcomeText = (TextView) findViewById(R.id.Home_WelcomeField);
 
@@ -33,9 +39,11 @@ public class Home_Activity extends AppCompatActivity implements View.OnClickList
             startActivity(logInActivity);
         }
 
+        joinCollBtn.setOnClickListener(this);
         FirebaseUser user = firAuth.getCurrentUser();
         welcomeText.setText("Welcome " + user.getEmail());
         logOutBtn.setOnClickListener(this);
+
     }
 
     @Override
@@ -45,6 +53,28 @@ public class Home_Activity extends AppCompatActivity implements View.OnClickList
             firAuth.signOut();
             finish();
             startActivity(logInActivity);
+        }
+        else if (v == joinCollBtn){ //FIX ME IF NOT FIXED
+            addCollectiveDia = new AlertDialog.Builder(Home_Activity.this);
+            mView = getLayoutInflater().inflate(R.layout.home_activity_addcol, null);
+            final EditText colIDF = (EditText) mView.findViewById(R.id.homeAddCol_colIDField);
+            final Button joinCol = (Button) mView.findViewById(R.id.homeAddCol_joinColBtn);
+            final Button createCol = (Button) mView.findViewById(R.id.homeAddCol_createColBtn);
+            joinCol.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+            createCol.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+            addCollectiveDia.setView(mView);
+            AlertDialog dialog = addCollectiveDia.create();
+            dialog.show();
         }
     }
 }
